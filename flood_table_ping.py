5f1b14d38718beca021262b288eb0ac732b6718f
Ante Skoric <ante.skoric@haw-hamburg.de>
2020-11-23 15:52:38 +0100

    Added flood_table with the ping command

---
+++ flood_table_ping.py
@@ -0,0 +1,17 @@
+import os
+import random as rm
+import socket
+import struct
+
+#The packages will be loss because the network will filter the ips that are not in the network
+call_size = 50
+dest_ips = ["10.0.0.1", "10.0.0.2", "10.0.0.3", "10.0.0.4", "10.0.0.5", "10.0.0.6", "10.0.0.8", "10.0.0.18", "10.0.0.42", "10.0.0.43",
+       "10.0.0.44"]
+sudoPassword = ''
+
+for x in range(0, call_size):
+    random_src_ip = socket.inet_ntoa(struct.pack('>I', rm.randint(1, 0xffffffff)))
+    for i in range(0, 10):
+        dest_ip = dest_ips[i]
+        #"hping3 -c " + packages + " -p 7 " + "-d " + data_size + " --rand-source " + " " + dest_ip
+        os.system('echo %s|sudo -S %s' % (sudoPassword, "ping -I " + random_src_ip + " " + dest_ip))
