6cefd93d957e360916db795317cf214e32eeb140
Ante Skoric <ante.skoric@haw-hamburg.de>
2020-12-05 13:51:48 +0100

    Fixed the script

--- link_flooding_two.py
+++ link_flooding_two.py
@@ -5,7 +5,7 @@ dest_ips_left = ["10.0.0.1", "10.0.0.2", "10.0.0.6", "10.0.0.8", "10.0.0.42"]
 dest_ips_right = ["10.0.0.3", "10.0.0.4", "10.0.0.5"]
 sudoPassword = ''
 
-for i in range(0, len(dest_ips_left) - 1):
+for i in range(0, len(dest_ips_right) - 1):
     # dest_io = dest_ips_right[i]
     dest_ip = dest_ips_left[i]
     sourc_ip = dest_ips_right[i]
