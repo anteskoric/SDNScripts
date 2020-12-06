3075a53eb5a59b027a2342a3b6ee227c51ab76b9
Ante Skoric <ante.skoric@haw-hamburg.de>
2020-12-06 13:44:10 +0100

    Added new flooding script that attacks only one host

---
+++ flood_table_one_dest_host.py
@@ -0,0 +1,8 @@
+import os
+
+packages = "1000"
+data_size = "5"
+dest_ip = "10.0.0.3"
+sudoPassword = ''
+
+os.system('echo %s|sudo -S %s' % (sudoPassword, "hping3 -c " + packages + " --icmp -p 7 " + "-d " + data_size + " --rand-source " + dest_ip))
