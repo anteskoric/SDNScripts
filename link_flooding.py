49e0f130ca5600ed9a00ea50b036f2b35e804ea6
Ante Skoric <ante.skoric@haw-hamburg.de>
2021-01-04 15:07:41 +0100

    Added comments to the link_flooding script

--- link_flooding.py
+++ link_flooding.py
@@ -1,6 +1,8 @@
 import os
 
 # This script is working, needs to be tested
+# This rout needs to exist before the script can be executed
+# sudo route -n add -net 224.0.2.4 netmask 255.255.255.255 dev eno1
 packages = "2000"
 data_size = "120"
 sourc_ip = "10.0.0.42"
